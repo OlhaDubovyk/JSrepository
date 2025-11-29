17e96b9039a7a90e2f4d24b8c882eb6ac9397653
OlhaDubovyk <sinoritaolga@gmail.com>
2025-11-29 16:16:02 +0100

    HW5.1

---
+++ HW5.1/script.js
@@ -0,0 +1,14 @@
+function pow(x, y) {
+  let result = 1;
+  for (let i = 0; i < y; i++) {
+    let temp = 0;
+    for (let j = 0; j < x; j++) {
+      temp = temp + result;
+    }
+    result = temp;
+  }
+  return result;
+}
+
+console.log(pow(2, 3));
+
